d40b5368b071cef6dfa322123eba1729111b5dc0
Keks <keks@htmlacademy.ru>
2015-11-11 17:53:44 +0300

    :heavy_check_mark: Сборка #2

---
+++ 2/js/check.js
@@ -0,0 +1,40 @@
+  'use strict';
+  function getMessage (a, b) {
+
+    if(a === true) {
+      return ('Я попал в ' + b);
+    } else if (a === false) {
+      return 'Я никуда не попал';
+    }
+
+    if(typeof a === 'number') {
+      return 'Я прыгнул на ' + (a * 100) + ' сантиметров';
+    }
+
+    if((typeof a === 'object') && (typeof b !== 'object')) {
+      var sum = a.reduce(function(count, current) {
+        return count + current;
+      })
+      return 'Я прошел ' + sum + ' шагов';
+    }
+
+    if((typeof a === 'object') && (typeof b === 'object')) {
+      while(a.length > b.length) {
+        b.push(0);
+      }
+
+      while(a.length < b.length) {
+        a.push(0);
+      }
+
+      var length = a.map(function(value, index) {
+        return (value * b[index]);
+      })
+
+      length = length.reduce(function(count, current) {
+        return count + current;
+      })
+      return 'Я прошел ' + length + ' метров';
+    }
+
+  };
