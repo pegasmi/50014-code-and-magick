0e6128e0290c55dae7e93ad3f5ddaa99e5045efc
Natalia Yorkina <yorkinan@gmail.com>
2015-11-26 16:37:09 +0300

    validation form first commit

--- js/form.js
+++ js/form.js
@@ -4,6 +4,10 @@
   var formContainer = document.querySelector('.overlay-container');
   var formOpenButton = document.querySelector('.reviews-controls-new');
   var formCloseButton = document.querySelector('.review-form-close');
+  var formMarks = document.querySelectorAll('input[name="review-mark"]');
+  var formReviewText = document.querySelector('#review-text');
+  var formReviewName = document.querySelector('#review-name');
+  var reviewFieldsName = document.querySelector('.review-fields-name');
 
   formOpenButton.onclick = function(evt) {
     evt.preventDefault();
@@ -14,4 +18,21 @@
     evt.preventDefault();
     formContainer.classList.add('invisible');
   };
+
+  for (var i = 0; formMarks.length > i; i++) {
+    formMarks[i].addEventListener('change', function(evt) {
+      if (evt.target.value < 4) {
+        formReviewText.required = true;
+      } else {
+        formReviewText.required = false;
+      }
+    });
+  }
+
+  formReviewName.addEventListener('change', function() {
+    if (formReviewName.value) {
+      reviewFieldsName.classList.add('invisible');
+    }
+  });
+
 })();
