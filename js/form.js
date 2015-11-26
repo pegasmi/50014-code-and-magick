1a4dcbbdb0a1510e7903010118ce534ea6edfb56
Natalia Yorkina <yorkinan@gmail.com>
2015-11-26 22:12:16 +0300

    second time

--- js/form.js
+++ js/form.js
@@ -8,31 +8,73 @@
   var formReviewText = document.querySelector('#review-text');
   var formReviewName = document.querySelector('#review-name');
   var reviewFieldsName = document.querySelector('.review-fields-name');
+  var reviewFieldsText = document.querySelector('.review-fields-text');
+  var reviewSubmitBtn = document.querySelector('.review-submit');
 
   formOpenButton.onclick = function(evt) {
     evt.preventDefault();
-    formContainer.classList.remove('invisible');
+    elementHidden(formContainer);
   };
 
   formCloseButton.onclick = function(evt) {
     evt.preventDefault();
-    formContainer.classList.add('invisible');
+    elementShow(formContainer);
   };
+
+  formReviewText.required = true;
 
   for (var i = 0; formMarks.length > i; i++) {
     formMarks[i].addEventListener('change', function(evt) {
       if (evt.target.value < 4) {
         formReviewText.required = true;
+        elementHidden(reviewFieldsText);
+        buttonTogler();
       } else {
         formReviewText.required = false;
+        elementShow(reviewFieldsText);
+        buttonTogler();
       }
     });
   }
 
   formReviewName.addEventListener('change', function() {
-    if (formReviewName.value) {
-      reviewFieldsName.classList.add('invisible');
+    if (this.value && this.value.match(/^[А-Яа-яЁё\s]+$/)) {
+      elementShow(reviewFieldsName);
+      buttonTogler();
+    } else {
+      this.value = '';
+      elementHidden(reviewFieldsName);
+      buttonTogler();
     }
   });
+
+  formReviewText.addEventListener('change', function() {
+    if (this.value && this.value.match(/^[А-Яа-яЁё\s]+$/)) {
+      elementShow(reviewFieldsText);
+      buttonTogler();
+    } else {
+      this.value = '';
+      elementHidden(reviewFieldsText);
+      buttonTogler();
+    }
+  });
+
+  function buttonTogler() {
+    if (formReviewText.required && formReviewText.value) {
+      reviewSubmitBtn.disabled = false;
+    } else if (formReviewText.required && formReviewText.value !== false) {
+      reviewSubmitBtn.disabled = true;
+    } else if (formReviewText.required === false) {
+      reviewSubmitBtn.disabled = false;
+    }
+  }
+
+  function elementShow(el) {
+    el.classList.add('invisible');
+  }
+
+  function elementHidden(el) {
+    el.classList.remove('invisible');
+  }
 
 })();
