003703fb21e939cedecd2e7c5eea02a63b826be2
Keks <keks@htmlacademy.ru>
2015-11-29 18:08:15 +0300

    :heavy_check_mark: Сборка #6

--- 6/js/form.js
+++ 6/js/form.js
@@ -1,94 +1,60 @@
 'use strict';
 
 (function() {
+  //попап с формой обратной связи
   var formContainer = document.querySelector('.overlay-container');
   var formOpenButton = document.querySelector('.reviews-controls-new');
   var formCloseButton = document.querySelector('.review-form-close');
-  var formReviewText = document.querySelector('#review-text');
-  var formReviewName = document.querySelector('#review-name');
-  var formReviewFields = document.querySelector('.review-fields');
-  var formReviewLabels = document.querySelectorAll('.review-fields-label');
-  var reviewFields = document.querySelectorAll('.review-form-field');
-  var reviewFieldsName = document.querySelector('.review-fields-name');
-  var reviewFieldsText = document.querySelector('.review-fields-text');
-  var reviewSubmitBtn = document.querySelector('.review-submit');
+
+  //элементы формы и форма
   var reviewForm = document.querySelector('.review-form');
-  var reviewFormElements = reviewForm.elements.namedItem('review-mark');
+  var reviewFormMarks = reviewForm.elements.namedItem('review-mark');
+  var fieldReview = reviewForm.querySelector('#review-text');
+  var fieldName = reviewForm.querySelector('#review-name');
+
+  //появление блока с информацией о заполнении
+  var notificationContainer = reviewForm.querySelector('.review-fields');
+  var notificationName = reviewForm.querySelector('.review-fields-name');
+  var notificationReview = reviewForm.querySelector('.review-fields-text');
+
+  //Кнопка отправки
+  var reviewSubmitBtn = reviewForm.querySelector('.review-submit');
 
   formOpenButton.onclick = function(evt) {
     evt.preventDefault();
-    elementShow(formContainer);
+    formContainer.classList.remove('invisible');
   };
 
   formCloseButton.onclick = function(evt) {
     evt.preventDefault();
-    elementHide(formContainer);
+    formContainer.classList.add('invisible');
   };
 
-  for (var i = 0; reviewFormElements.length > i; i++) {
-    reviewFormElements[i].addEventListener('change', function(evt) {
-      if (parseInt(evt.target.value, 10) < 3) {
-        formReviewText.required = true;
-        reviewSubmitBtn.disabled = formValid();
-      } else {
-        formReviewText.required = false;
-        reviewSubmitBtn.disabled = formValid();
-      }
-    });
+  function reviewValidate() {
+    var badMark = reviewFormMarks.value < 3;
+    var nameValue = !fieldName.value;
+    var reviewValue = badMark && !fieldReview.value;
+    return [
+      [notificationName, !nameValue],
+      [notificationReview, !reviewValue],
+      [notificationContainer, !nameValue && !reviewValue]
+    ];
   }
 
-  function formValid() {
-    if (formReviewText.value && formReviewName.value) {
-      elementHide(formReviewFields);
-      return false;
-    }
-    if (formReviewText.value && !formReviewName.value) {
-      reviewClassClear();
-      elementShow(formReviewFields);
-      elementHide(reviewFieldsText);
-      return true;
-    }
-    if (formReviewText.required && !formReviewText.value && formReviewName.value) {
-      reviewClassClear();
-      elementShow(formReviewFields);
-      elementHide(reviewFieldsName);
-      return true;
-    }
-    if (formReviewText.required && !formReviewText.value && !formReviewName.value) {
-      elementShow(formReviewFields);
-      return true;
-    }
-    if (!formReviewText.required && !formReviewText.value && !formReviewName.value) {
-      reviewClassClear();
-      elementShow(formReviewFields);
-      elementHide(reviewFieldsText);
-      return true;
-    }
-    if (!formReviewText.required && formReviewName.value) {
-      elementHide(formReviewFields);
-      return false;
+  function showElement(arr) {
+    if (arr[1]) {
+      arr[0].classList.add('invisible');
+    } else {
+      arr[0].classList.remove('invisible');
     }
   }
 
-  function reviewClassClear() {
-    for (var j = 0; formReviewLabels.length > j; j++) {
-      formReviewLabels[j].classList.remove('invisible');
-    }
+  function changeHandler() {
+    reviewValidate().forEach(showElement);
+    reviewSubmitBtn.disabled = !notificationContainer.classList.contains('invisible');
   }
 
-  for (var h = 0; reviewFields.length > h; h++) {
-    reviewFields[h].addEventListener('change', function() {
-      reviewSubmitBtn.disabled = formValid();
-    });
-  }
-
-  function elementHide(el) {
-    el.classList.add('invisible');
-  }
-
-  function elementShow(el) {
-    el.classList.remove('invisible');
-  }
+  reviewForm.addEventListener('change', changeHandler);
+  changeHandler();
 
 })();
-
