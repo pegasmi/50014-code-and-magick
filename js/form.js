e2d003afe2d3f3b6c039435f5b8735e21eaa1060
Natalia Yorkina <yorkinan@gmail.com>
2015-12-02 09:28:04 +0300

    add cookies

--- js/form.js
+++ js/form.js
@@ -4,6 +4,11 @@
   var formContainer = document.querySelector('.overlay-container');
   var formOpenButton = document.querySelector('.reviews-controls-new');
   var formCloseButton = document.querySelector('.review-form-close');
+  //var submitButton = document.querySelector('.review-submit');
+  var form = document.querySelector('.review-form');
+  var mark = form.elements.namedItem('review-mark');
+  var user = document.querySelector('.review-form-field-name');
+
 
   formOpenButton.onclick = function(evt) {
     evt.preventDefault();
@@ -14,4 +19,30 @@
     evt.preventDefault();
     formContainer.classList.add('invisible');
   };
+
+  form.addEventListener('submit', function(evt) {
+    evt.preventDefault();
+    docCookies.setItem('checked-mark', mark.value);
+    docCookies.setItem('user-name', user.value);
+    docCookies.setItem('expires', getCookieLife());
+    formContainer.classList.add('invisible');
+  });
+
+  function getCookieLife() {
+    var now = +Date.now();
+    var myLastBirthday = new Date('2015, 03, 28');
+    var myLastBirthdayInSec = +myLastBirthday;
+    var tillLastBirthday = now - myLastBirthdayInSec;
+    var result = new Date(tillLastBirthday);
+    return result;
+  }
+
+  var restoreCookies = function() {
+    if (docCookies.hasItem('сhecked-mark') && docCookies.hasItem('user-name')) {
+      mark.value = docCookies.getItem('сhecked-mark');
+      user.value = docCookies.getItem('user-name');
+    }
+  };
+  restoreCookies();
 })();
+
