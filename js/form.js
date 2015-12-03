0b271764eb53a763caecd9c5a0bd850d2767f03b
Natalia Yorkina <yorkinan@gmail.com>
2015-12-03 22:00:38 +0300

    cookies 1.1

--- js/form.js
+++ js/form.js
@@ -4,7 +4,6 @@
   var formContainer = document.querySelector('.overlay-container');
   var formOpenButton = document.querySelector('.reviews-controls-new');
   var formCloseButton = document.querySelector('.review-form-close');
-  //var submitButton = document.querySelector('.review-submit');
   var form = document.querySelector('.review-form');
   var mark = form.elements.namedItem('review-mark');
   var user = document.querySelector('.review-form-field-name');
@@ -13,6 +12,7 @@
   formOpenButton.onclick = function(evt) {
     evt.preventDefault();
     formContainer.classList.remove('invisible');
+    restoreCookies();
   };
 
   formCloseButton.onclick = function(evt) {
@@ -22,27 +22,25 @@
 
   form.addEventListener('submit', function(evt) {
     evt.preventDefault();
-    docCookies.setItem('checked-mark', mark.value);
-    docCookies.setItem('user-name', user.value);
-    docCookies.setItem('expires', getCookieLife());
+    docCookies.setItem('checked-mark', mark.value, getCookieLife());
+    docCookies.setItem('user-name', user.value, getCookieLife());
     formContainer.classList.add('invisible');
   });
 
   function getCookieLife() {
     var now = +Date.now();
     var myLastBirthday = new Date('2015, 03, 28');
-    var myLastBirthdayInSec = +myLastBirthday;
-    var tillLastBirthday = now - myLastBirthdayInSec;
-    var result = new Date(tillLastBirthday);
+    var sinceMyLastBirthday = now - +myLastBirthday;
+    var cookiesDeleteDate = now + sinceMyLastBirthday;
+    var result = new Date(cookiesDeleteDate);
     return result;
   }
 
-  var restoreCookies = function() {
-    if (docCookies.hasItem('сhecked-mark') && docCookies.hasItem('user-name')) {
-      mark.value = docCookies.getItem('сhecked-mark');
-      user.value = docCookies.getItem('user-name');
-    }
-  };
-  restoreCookies();
+  function restoreCookies() {
+    user.value = docCookies.getItem('user-name');
+    var number = parseInt(docCookies.getItem('сhecked-mark'), 10) - 1;
+    var checkedMark = form.elements.namedItem('review-mark')[number];
+    checkedMark.checked = true;
+  }
 })();
 
