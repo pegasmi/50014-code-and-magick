5a7f59e85f9eebf9fffd789b9c0c24220fd8febd
Keks <keks@htmlacademy.ru>
2015-12-07 13:14:53 +0300

    :heavy_check_mark: Сборка #8

---
+++ 8/js/check.js
@@ -0,0 +1,32 @@
+  'use strict';
+/**
+ * @param {Array.<number>|number|boolean} a
+ * @param {Array.<number>|number|boolean} b
+ * @return {string} 
+ */
+  function getMessage (a, b) {
+    if(a === true) {
+      return 'Я попал в ' + b;
+    } else if(a === false) {
+      return 'Я никуда не попал';
+    }
+
+    if(typeof a === 'number') {
+      return 'Я прыгнул на ' + (a * 100) + ' сантиметров';
+    }
+
+    if(Array.isArray(a) && !Array.isArray(b)) {
+      var sum = a.reduce(function(count, current) {
+        return count + current;
+      });
+      return 'Я прошёл ' + sum + ' шагов';
+    }
+
+    if(Array.isArray(a) && Array.isArray(b)) {
+      var length = a.reduce(function(count, current, index) {
+        return count + current * (b[index] || 0);
+    }, 0);
+      return 'Я прошёл ' + length + ' метров';
+    }
+  return 'Ой';
+  };
