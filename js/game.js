bdea282aa1fdd7f370f5dc410594cc77d175c62e
Natalia Yorkina <yorkinan@gmail.com>
2015-12-20 13:30:49 +0300

    add hw module6-task2

--- js/game.js
+++ js/game.js
@@ -790,4 +790,30 @@
   var game = new Game(document.querySelector('.demo'));
   game.initializeLevelAndStart();
   game.setGameStatus(window.Game.Verdict.INTRO);
+
+  var scrollTimeout;
+  var demo = document.querySelector('.demo');
+  var clouds = document.querySelector('.header-clouds');
+  var moveClouds = true;
+
+  window.addEventListener('scroll', function() {
+    clearTimeout(scrollTimeout);
+    var topCoordinate = clouds.getBoundingClientRect().top;
+
+    if (moveClouds) {
+      clouds.style.backgroundPosition = topCoordinate + 'px';
+    }
+
+    scrollTimeout = setTimeout(function() {
+      if (clouds.getBoundingClientRect().bottom < 0) {
+        moveClouds = false;
+      } else {
+        moveClouds = true;
+      }
+      if (demo.getBoundingClientRect().bottom < 0) {
+        game.setGameStatus(window.Game.Verdict.PAUSE);
+      }
+    }, 100);
+  });
+
 })();
